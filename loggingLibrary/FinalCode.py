90fa0eea85ce90e1b146f13559b0df633d5e31c6
Mohamed Taher Othmen <mohamedtaher.othman@gmail.com>
2023-12-05 20:02:27 +0100

    FinalCode Created

--- loggingLibrary/FinalCode.py
+++ loggingLibrary/FinalCode.py
@@ -1,3 +1,16 @@
 # after two nights of hard learning I've master almost all logging's Labrary functions
 import logging
 
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('/home/taher/Python101/loggingLibrary/Logs/finallog.log')
+Format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger.setLevel(logging.DEBUG)
+handler.setLevel(logging.INFO)
+handler.setFormatter(Format)
+logger.addHandler(handler)
+
+logger.debug('This The Debug Message')
+logger.info('This The Info Message')
+logger.warning('This The Warning Message')
+logger.error('This The Error Message')
